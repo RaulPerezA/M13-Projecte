127cb7b8597552d836d68ab8fb30c7aaceab4012
jon2n1011 <48183794+jon2n1011@users.noreply.github.com>
2020-05-17 23:12:33 +0200

    17/05/20 Jon rutinas json

--- spring-mongodb/src/main/java/com/spring/mongodb/services/RutinaDiaServiceBD.java
+++ spring-mongodb/src/main/java/com/spring/mongodb/services/RutinaDiaServiceBD.java
@@ -2,13 +2,12 @@ package com.spring.mongodb.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.spring.mongodb.repository.RutinaDiaRepository;
+import com.spring.mongodb.repository.RutinaDiasRepository;
 
 @Service("RutinaDiaServiceBD")
 public class RutinaDiaServiceBD {
 	@Autowired
-	private RutinaDiaRepository repositori;
+	private RutinaDiasRepository repositori;
 }
 	
 
