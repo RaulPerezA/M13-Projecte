7355bac462951453acf1aa07ae8652db4fc19508
IvanMorenoMora97 <48072126+IvanMorenoMora97@users.noreply.github.com>
2020-05-23 00:18:45 +0200

    [UPDATE] 0:17 23/05/2020
    Las pantallas de las rutinas ya muestran si el usuario tiene creadas varias rutinas con sus ejerciciod diarios y los ejercicios que va ha  hacer.

--- Fitness App/FitnessApplication/src/app/editgeneral/editgeneral.page.ts
+++ Fitness App/FitnessApplication/src/app/editgeneral/editgeneral.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { RutinaDia } from '../Objects/RutinaDia';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-editgeneral',
@@ -8,11 +10,18 @@ import { NavController } from '@ionic/angular';
 })
 export class EditgeneralPage implements OnInit {
 
-  daily:string[]=["Diaria 1","Diaria 2","Diaria 3","Diaria 4","Diaria 5","Diaria 6","Diaria 7","Diaria 8","Diaria 9","Diaria 10"];
-
-  constructor(private navCtrol: NavController) { }
+  //daily:string[]=["Diaria 1","Diaria 2","Diaria 3","Diaria 4","Diaria 5","Diaria 6","Diaria 7","Diaria 8","Diaria 9","Diaria 10"];
+  daily:Array<RutinaDia>;
+  constructor(private navCtrol: NavController, private storage:Storage) { }
 
   ngOnInit() {
+    this.daily = new Array<RutinaDia>();
+    this.storage.get('dailyGeneral').then( general => {
+      for(let d of general.rutinasDias){
+        console.log("d",d);
+        this.daily.push(d);
+      }
+    });
   }
 
   editDaily() {
@@ -21,6 +30,10 @@ export class EditgeneralPage implements OnInit {
 
   addDaily() {
     this.navCtrol.navigateForward('/add-daily');
+  }
+
+  goToDaily() {
+    this.navCtrol.navigateForward('/listexercice');
   }
 
 }
--- Fitness App/FitnessApplication/src/app/listexercice/listexercice.page.ts
+++ Fitness App/FitnessApplication/src/app/listexercice/listexercice.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { RutinaDia } from '../Objects/RutinaDia';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-listexercice',
@@ -8,11 +10,21 @@ import { NavController } from '@ionic/angular';
 })
 export class ListexercicePage implements OnInit {
 
-  exercices:string[]=["RE1","RE2","RE3","RE4","RE5","RE6","RE7","RE8","RE9","RE10"];
-
-  constructor(private navCtrl: NavController) { }
+  //exercices:string[]=["RE1","RE2","RE3","RE4","RE5","RE6","RE7","RE8","RE9","RE10"];
+  exercices:Array<RutinaDia>;
+  constructor(private navCtrl: NavController, private storage:Storage) { }
 
   ngOnInit() {
+
+    this.exercices = new Array<RutinaDia>();
+    this.storage.get('dailyGeneral').then( general => {
+      for(let r of general.rutinasDias){
+         for(let e of r.ejercicios){
+           this.exercices.push(e);
+         }
+      }
+    });
+
   }
 
   createExercice() {
