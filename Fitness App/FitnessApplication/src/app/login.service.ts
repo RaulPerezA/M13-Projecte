b97518496d6e8d9bb4e78e41fdc4abc18a246832
jon2n1011 <48183794+jon2n1011@users.noreply.github.com>
2020-05-26 23:31:07 +0200

    26/05 /20 Encriptado de contraseña
    Encriptado de password

--- Fitness App/FitnessApplication/src/app/login.service.ts
+++ Fitness App/FitnessApplication/src/app/login.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import * as CryptoJS from 'crypto-js';
+declare var require: any
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,9 @@ export class LoginService {
 
   getLogin(username:string, password:string) {
     //username la inicial mayuscula, contraseña todo en minusculas
-    return this.http.get(this.LOGIN+"?user="+username+"&pw="+password);
+    // Hay que cifrar la contraseña para enviarlo de forma segura a la URL
+    var SHA256 = require("crypto-js/sha256");
+    return this.http.get(this.LOGIN+"?user="+username+"&pw="+SHA256(password).toString());
   }
 
   getLoginUser(email:string) {
